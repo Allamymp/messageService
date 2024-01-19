2adc7d6c004cdaa3e28cc1ae5148b78723a0d60c
allamymp <allamympereira@gmail.com>
2024-01-18 21:58:18 -0300

    client service created

--- src/main/java/org/portfolio/emailsender/service/ClientService.java
+++ src/main/java/org/portfolio/emailsender/service/ClientService.java
@@ -11,21 +11,40 @@ import java.util.Optional;
 
 @Service
 public class ClientService {
+
     @Autowired
     ClientRepository repository;
 
     public Optional<Client> getByID(Long id) {
+        return repository.findById(id);
     }
 
     public List<Client> getAll() {
+        return repository.findAll();
     }
 
     public void create(ClientRequestDTO data) {
+        Client client = new Client(data.name(), data.email(), data.password());
+        repository.save(client);
     }
 
     public void edit(ClientRequestDTO data) {
+        Optional<Client> clientOptional = repository.findById(data.id());
+        clientOptional.ifPresent(client -> {
+            if (!data.name().isBlank()) {
+                client.setName(data.name());
+            }
+            if (!data.email().isBlank()) {
+                client.setEmail(data.email());
+            }
+            if (!data.password().isBlank()) {
+                client.setPassword(data.password());
+            }
+            repository.save(client);
+        });
     }
 
-    public void delete(Optional<Client> client) {
+    public void delete(Optional<Client> clientOptional) {
+        clientOptional.ifPresent(repository::delete);
     }
 }
